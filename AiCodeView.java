bdaf52cf1950f6b8978c4c4d029be4e228ec538f
wujinji <kingdee@ZBBB2008C-0024.kingdee.gbl>
2026-03-23 18:44:36 +0800

    [FIX] test

---
+++ AiCodeView.java
@@ -0,0 +1,10 @@
+public class AiCodeView {
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 3));
+    }
+    
+    public static int add(int a, int b) {
+        return a - b;
+    }
+}
